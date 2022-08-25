52d1252114cee3ba493ccbed745b058d450d07c0
Masuo-4 <h.masuda@4nonome.com>
2022-08-26 05:05:18 +0900

    modified BeltConveyor

--- ABC265/C-BeltConveyor.py
+++ ABC265/C-BeltConveyor.py
@@ -1,7 +1,7 @@
 row_num, col_num = map(int, input().split())
 maze = []
 for i in range(row_num):
-    maze.append(input().split()[0])
+    maze.append(list(input().split()[0]))
 
 position_x = 0
 position_y = 0
@@ -15,28 +15,28 @@ while True:
         if position_y == 0:
             break
         else:
-            maze[position_y][position_x] == 0
+            maze[position_y][position_x] = 0
             position_y -= 1
 
     elif maze[position_y][position_x] == "D":
         if position_y == row_num - 1:
             break
         else:
-            maze[position_y][position_x] == 0
+            maze[position_y][position_x] = 0
             position_y += 1
 
     elif maze[position_y][position_x] == "R":
         if position_x == col_num - 1:
             break
         else:
-            maze[position_y][position_x] == 0
+            maze[position_y][position_x] = 0
             position_x += 1
 
     elif maze[position_y][position_x] == "L":
         if position_x == 0:
             break
         else:
-            maze[position_y][position_x] == 0
+            maze[position_y][position_x] = 0
             position_x -= 1
 
 print(position_y + 1, position_x + 1)

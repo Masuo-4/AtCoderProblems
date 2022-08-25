1f37c8332bc00efeabd0580ee742e4710fae0422
Masuo-4 <h.masuda@4nonome.com>
2022-08-26 04:53:50 +0900

    BeltConveyor

---
+++ ABC265/C-BeltConveyor.py
@@ -0,0 +1,42 @@
+row_num, col_num = map(int, input().split())
+maze = []
+for i in range(row_num):
+    maze.append(input().split()[0])
+
+position_x = 0
+position_y = 0
+
+while True:
+    if maze[position_y][position_x] == 0:
+        print(-1)
+        exit()
+
+    elif maze[position_y][position_x] == "U":
+        if position_y == 0:
+            break
+        else:
+            maze[position_y][position_x] == 0
+            position_y -= 1
+
+    elif maze[position_y][position_x] == "D":
+        if position_y == row_num - 1:
+            break
+        else:
+            maze[position_y][position_x] == 0
+            position_y += 1
+
+    elif maze[position_y][position_x] == "R":
+        if position_x == col_num - 1:
+            break
+        else:
+            maze[position_y][position_x] == 0
+            position_x += 1
+
+    elif maze[position_y][position_x] == "L":
+        if position_x == 0:
+            break
+        else:
+            maze[position_y][position_x] == 0
+            position_x -= 1
+
+print(position_y + 1, position_x + 1)

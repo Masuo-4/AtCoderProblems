3d333fa6482c99f6b208950bdb0c21da6979d86b
Masuo-4 <h.masuda@4nonome.com>
2022-08-27 05:34:01 +0900

    modified Ancestor

--- ABC263/B-ancestor.py
+++ ABC263/B-ancestor.py
@@ -1,9 +1,10 @@
+# 訂正版(動的計画法により計算量O(N)で済む)
+
 n = int(input())
 num_list = list(map(int, input().split()))
 
-ans = 0
-while n != 1:
-	n = num_list[n - 2]
-	ans += 1
+dp = [0] * (n + 1)
+for i in range(2, n + 1):
+	dp[i] = dp[num_list[i - 2]] + 1
 
-print(ans)
+print(dp)

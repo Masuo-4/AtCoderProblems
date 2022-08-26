ea64df9f6cd8c083d08cb3a71b4c43256ec0506d
Masuo-4 <h.masuda@4nonome.com>
2022-08-27 07:20:30 +0900

    計算量削減

--- ABC262/B-Triangle.py
+++ ABC262/B-Triangle.py
@@ -1,25 +1,19 @@
 import itertools
-
-# 入力
 vertex, edge_num = map(int, input().split())
-pair = []
+adj = [[False] * vertex for _ in range(vertex)]
+
 for i in range(edge_num):
-	pair.append(list(map(int, input().split())))
+	u, v = map(int, input().split())
+	adj[u - 1][v - 1] = True
+	adj[v - 1][u - 1] = True
 
-# 頂点の組み合わせのリスト作成
-edge_lst = [[] for _ in range(vertex)]
-for i in pair:
-	edge_lst[i[0] - 1].append(i[1])
-	edge_lst[i[1] - 1].append(i[0])
-for lst in edge_lst:
-    lst.sort()
-
-# 頂点の組み合わせを全部調べる
 count = 0
-num = [i for i in range(1, vertex + 1)]
-for v in itertools.combinations(num, 3):
-    if v[1] in edge_lst[v[0] - 1] and v[2] in edge_lst[v[0] - 1]:
-        if v[2] in edge_lst[v[1] - 1]:
-            count += 1
-            
+lst = [i for i in range(1, vertex + 1)]
+for i, j, k in itertools.combinations(lst, 3):
+	i -= 1
+	j -= 1
+	k -= 1
+	if adj[i][j] and adj[i][k] and adj[k][j]:
+    	 count += 1
+
 print(count)

ffdcadcba12ad34f2fbeeed57ee4a908c5b920fa
Masuo-4 <h.masuda@4nonome.com>
2022-09-01 06:42:41 +0900

    modified Rotation

--- ABC258/C-Rotation.py
+++ ABC258/C-Rotation.py
@@ -1,16 +1,13 @@
-def query(s, t, x):
-   if t == 1:
-        s = s[-x:] + s[:-x]
-        return s
-   else:
-       print(s[x - 1])
-
 n, q = list(map(int, input().split()))
 s = input()
 
 query_lst = []
 for i in range(q):
-    query_lst.append(list(map(int, input().split()))) 
+    query_lst.append(list(map(int, input().split())))
 
+count = 0
 for t, x in query_lst:
-    query(s, t, x)
+    if t == 1:
+        count += x
+    else:
+        print(s[(x - count - 1) % n])
